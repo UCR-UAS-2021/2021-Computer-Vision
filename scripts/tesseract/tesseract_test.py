5c33de682ec73d9c17928a402141bd83863ea8dc
hscottvo <hscottv@gmail.com>
2020-10-19 14:44:15 -0700

    Added compatibility for running on different systems based on directory systems

--- scripts/tesseract/tesseract_test.py
+++ scripts/tesseract/tesseract_test.py
@@ -3,7 +3,9 @@ import pytesseract
 import cv2
 import os
 
-image_path = os.path.expanduser("~/Development/2021-Computer-Vision/scripts/tesseract/image.png")
+repo_path = '~/github/2021-Computer-Vision'
+image_path = os.path.expanduser(os.path.join(repo_path, "scripts/tesseract/0.png"))
+print(image_path)
 
 image = cv2.imread(image_path)
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -11,6 +13,8 @@ cv2.imwrite("gray.png".format(os.getpid()), gray)
 
 text = pytesseract.image_to_string(Image.open("gray.png"), lang='eng', config='--psm 7')
 os.remove("gray.png")
+if len(text) == 0:
+    print('no letters found')
 print(text)
 
 cv2.imshow("Gray", gray)
