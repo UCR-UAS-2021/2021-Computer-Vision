8b32638175ef42e69dac111162294ea23498b336
Kashyap Panda <5233012+kpanda254@users.noreply.github.com>
2020-11-10 12:57:42 -0800

    Fixed bugs

--- scripts/odlc/odlc_test.py
+++ scripts/odlc/odlc_test.py
@@ -1,7 +1,6 @@
 import os
 import cv2
 import numpy as np
-from matplotlib import pyplot as plt
 
 # If anybody else is running this, this should be changed
 image_path = os.path.expanduser("~/Development/2021-Computer-Vision/scripts/odlc/image.jpg")
@@ -11,13 +10,16 @@ frame = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # Canny Edge Detection seems to work really well on its own for filtering the letter/shape
-edges = cv2.Canny(frame, 200, 300)
+edges = cv2.Canny(frame, 100, 300)
 cv2.imshow('Canny Edge Detection', edges)
 
 # Otsu thresholding
-ret, thresh = cv2.threshold(img, 0, 255, cv2.THRESH_OTSU)
+ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 cv2.imshow('Otsu', thresh)
 
 # bilateral filtering
 bilateral = cv2.bilateralFilter(img, 20, 100, 100)
 cv2.imshow('Bilateral', bilateral)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
