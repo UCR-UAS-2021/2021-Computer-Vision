1ef70931da40ee28fb9e04f88f6e58cb5445c4e5
Kashyap Panda <5233012+kpanda254@users.noreply.github.com>
2020-11-10 13:21:37 -0800

    Added dilation to the canny edge detection

--- scripts/odlc/odlc_test.py
+++ scripts/odlc/odlc_test.py
@@ -9,9 +9,7 @@ img = cv2.imread(image_path)
 frame = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# Canny Edge Detection seems to work really well on its own for filtering the letter/shape
-edges = cv2.Canny(frame, 100, 300)
-cv2.imshow('Canny Edge Detection', edges)
+cv2.imshow('Base Image', img)
 
 # Otsu thresholding
 ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
@@ -20,6 +18,15 @@ cv2.imshow('Otsu', thresh)
 # bilateral filtering
 bilateral = cv2.bilateralFilter(img, 20, 100, 100)
 cv2.imshow('Bilateral', bilateral)
+
+# Canny Edge Detection seems to work really well on its own for filtering the letter/shape
+edges = cv2.Canny(frame, 200, 300)
+cv2.imshow('Canny Edge Detection', edges)
+
+# Dilation + Canny edge detection 
+kernel = np.ones((7, 7))
+dilation = cv2.dilate(edges,kernel,iterations = 1)
+cv2.imshow('dilation', dilation)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
