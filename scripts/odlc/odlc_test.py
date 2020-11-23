4010bba7ac5afe682fe952b70ea4180a860c064d
hscottvo <hscottvo@gmail.com>
2020-11-22 19:01:10 -0800

    Update odlc_test.py
    * Added max area to contour area
    * Added getting location from each contour and cropping around it

--- scripts/odlc/odlc_test.py
+++ scripts/odlc/odlc_test.py
@@ -3,7 +3,8 @@ import cv2
 import numpy as np
 
 # If anybody else is running this, this should be changed
-image_path = "../../../Image-Generator/IMG_0602.JPG"
+# image_path = "../../../Image-Generator/IMG_0602.JPG"
+image_path = "../../../Image-Generator/Images/0.png"
 
 img = cv2.imread(image_path)
 frame = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -47,13 +48,15 @@ contours, hierarchy = cv2.findContours(dilation, cv2.RETR_TREE, cv2.CHAIN_APPROX
 print(len(contours))
 # cv2.drawContours(contour_img, contours, -1, (0, 255, 0), 3)
 for c in contours:
-    if cv2.contourArea(c) <= 1000:
+    if cv2.contourArea(c) <= 1000 or cv2.contourArea(c) >= 6000:
         continue
     x, y, w, h = cv2.boundingRect(c)
     cv2.rectangle(contour_img, (x, y), (x + w, y + h), (0, 255, 0), 2)
     center = (x, y)
     print(center)
+    cv2.imshow('contour ' + str(center), img[y:y+h, x:x+h])
 cv2.imshow('contour', cv2.resize(contour_img, (img.shape[1] // 3, img.shape[0] // 3)))
+
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
