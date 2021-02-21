2bdff875a7c1f86a2f474af98514a821097c4ba9
hscottvo <hscottvo@gmail.com>
2021-02-21 03:27:04 -0800

    Refactor python files
    Turned out we are using less files than intended - have 1 folder for all odlc

--- odlc/contour.py
+++ odlc/contour.py
@@ -1,15 +1,16 @@
 import cv2
 
 
-def find_contours(img):
+def find_contours(img, draw):
     contours, hierarchy = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     # print(len(contours))
     # cv2.drawContours(contour_img, contours, -1, (0, 255, 0), 3)
     valid_contours = []
-    for c in contours:
-        if cv2.contourArea(c) <= 500 or cv2.contourArea(c) >= 6000:
+    print(len(contours))
+    for i in range(len(contours)):
+        if cv2.contourArea(contours[i]) <= 500 or cv2.contourArea(contours[i]) >= 6000:
             continue
-        x, y, w, h = cv2.boundingRect(c)
+        x, y, w, h = cv2.boundingRect(contours[i])
         if w > h * 1.5 or w < h / 1.5:
             continue
         valid_contours.append({'x': x,
@@ -17,6 +18,8 @@ def find_contours(img):
                                'w': w,
                                'h': h
                                })
+        draw = cv2.drawContours(draw, contours, i, (0, 255, 0), 3)
+        cv2.imshow('bruh', draw)
         # cv2.rectangle(orig_img, (x, y), (x + w, y + h), (0, 255, 0), 2)
         # center = (x, y)
         # print(center)
