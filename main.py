972aabee39bbf3b65f2f33c7f72961bbff4717cc
hscottvo <hscottvo@gmail.com>
2021-02-21 04:35:46 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import cv2
-from detection import detector
-from detection.preprocessing.blur import blur_image
-from detection.preprocessing.bitmap import find_edge
-from detection.preprocessing.bitmap import morphology
-from detection.preprocessing.contour import find_contours
-from classify import classifier
+import numpy as np
+import matplotlib.pyplot as plt
+from odlc import detector
+from odlc.classify import classifier
+from sklearn.cluster import KMeans
 
 
 def target_exists(target, target_list):
@@ -24,27 +23,21 @@ if __name__ == "__main__":
     target_list = []
     while (True):
         # ret, img = cap.read()
-        img = cv2.imread('./data/images/1.png')
-        draw_img = img
+        img = cv2.imread('./data/images/4.png')
+        draw_img = img.copy()
 
-        contour_list = detector.detect_targets(img)
+        contour_list, contours = detector.detect_targets(img)
         img_list = []
         for i in contour_list:
             img_list.append(img[i['y']:i['y']+i['h'], i['x']:i['x']+i['w']])
+
         for image, contour in zip(img_list, contour_list):
             curr_target = classifier.classify_target(image, contour)
             if not target_exists(curr_target, target_list):
                 target_list.append(curr_target)
-        # Display the resulting frame
-        # cv2.imshow('bruh', img)
-        # cv2.imshow('contours', draw_img)
-        # cv2.imshow('binary', morph)
-        # for i in target_list:
-        #     i.print()
-        # print(target_list[0].shape)
-        # cv2.imshow('morphology', morph)
-        for image in range(len(img_list)):
-            cv2.imshow(str(image), img_list[image])
+        # print(target_list[0].shape.name)
+        cv2.imshow('please', img)
+        print(contour_list)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
