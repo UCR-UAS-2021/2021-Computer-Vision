0eef55e1f2419adc65b5caa9e744822478410df2
hscottvo <hscottvo@gmail.com>
2021-02-21 03:58:38 -0800

    Update bitmap.py
    Improved edge detection by using otsu's method to get thresholds

--- odlc/bitmap.py
+++ odlc/bitmap.py
@@ -3,16 +3,19 @@ import numpy as np
 
 
 def find_edge(img):
-    return cv2.Canny(img, 325, 200)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    high_thresh, thresh_img = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    return cv2.Canny(img, high_thresh//2, high_thresh)
 
 
 def morphology(img):
-    kernel = np.ones((19, 19))
-    # return cv2.dilate(img, kernel, iterations=1)
-    return img
+    kernel = np.ones((3, 3))
+    return cv2.dilate(img, kernel, iterations=1)
+    # return img
 
 def floodfill(img):
     im_floodfill = img.copy()
+    im_floodfill = morphology(im_floodfill)
 
     h, w = img.shape[:2]
     mask = np.zeros((h+2, w+2), np.uint8)
