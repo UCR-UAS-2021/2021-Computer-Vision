e49e8b64803027b619c261c6a27e0044db4c112b
hscottvo <hscottvo@gmail.com>
2021-03-02 23:38:14 -0800

    Update main.py
    * Cleaned main program loop code
    * Adjusted for once-per-image ODLC
    * Future iterations may add real-time ODLC with mp4 or live webcam feed
    * Added more specific cropped-image naming convention

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import cv2
-import os
+import os, shutil
+from tqdm import tqdm
 import numpy as np
 import matplotlib.pyplot as plt
 from odlc import detector
 from odlc.classify import classifier
 from sklearn.cluster import KMeans
+
+images_path = './cropped_images'
+source_path = './data/Images'
 
 
 def target_exists(target, target_list):
@@ -16,37 +20,24 @@ def target_exists(target, target_list):
     return False
 
 
-if __name__ == "__main__":
-    # cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)
-
-    # cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
-    # cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
-    if not os.path.exists('./cropped_images'):
-        os.makedirs('./cropped_images')
-    target_list = []
-    while (True):
-        # ret, img = cap.read()
-        img = cv2.imread('./data/images/4.png')
-        draw_img = img.copy()
-        contour_list, contours = detector.detect_targets(img)
-        img_list = []
+def odlc_from_dir(file_path, img_path):
+    img_list = []
+    file_list = []
+    for img in tqdm(os.listdir(file_path)):
+        image = cv2.imread(os.path.join(file_path, img))
+        contour_list, contours = detector.detect_targets(image)
+        cnt = 0
         for i in contour_list:
-            img_list.append(img[i['y']:i['y']+i['h'], i['x']:i['x']+i['w']])
+            img_list.append(image[i['y']:i['y']+i['h'], i['x']:i['x']+i['w']])
+            file_list.append(img[:-4] + '-' + str(cnt))
+            cnt += 1
+    for i in range(len(img_list)):
+        cv2.imwrite(img_path + '/' + file_list[i] + '.png', img_list[i])
 
-        for image, contour in zip(img_list, contour_list):
-            curr_target = classifier.classify_target(image, contour)
-            if not target_exists(curr_target, target_list):
-                target_list.append(curr_target)
-        # print(target_list[0].shape.name)
-        cv2.imshow('img', img)
 
-        for i in range(len(img_list)):
-            cv2.imshow('img no. ' + str(i), img_list[i])
-            cv2.imwrite('./cropped_images/' + str(i) + '.png', img_list[i])
+if __name__ == "__main__":
+    if os.path.exists(images_path):
+        shutil.rmtree(images_path)
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    # When everything done, release the capture
-    # cap.release()
-    cv2.destroyAllWindows()
+    os.makedirs(images_path)
+    odlc_from_dir(source_path, images_path)
